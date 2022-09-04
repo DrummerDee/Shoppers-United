06d403ba6ab70fd13cab842ed223d77001a03bc4
Drummer Dee <drummer.deja@gmail.com>
2022-09-04 15:10:18 -0400

    changed background

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth'); 
 const homeController = require('../controllers/home');
-const forgotController = require('../controllers/forgot');
-const forgotPostController = require('../controllers/forgotPost');
+// const forgotController = require('../controllers/forgot');
+// const forgotPostController = require('../controllers/forgotPost');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 router.get('/', homeController.getIndex);
@@ -12,8 +12,8 @@ router.post('/login', authController.postLogin);
 router.get('/logout', authController.logout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
-router.get('/forgot-password', forgotController.getForgot);
-router.post('/forgot-password', forgotPostController.postForgot);
+// router.get('/forgot-password', forgotController.getForgot);
+// router.post('/forgot-password', forgotPostController.postForgot);
 
 
 
