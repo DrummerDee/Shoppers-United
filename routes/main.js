d4dc31780e20f77e4a8b89415fd38c325cb55e06
Drummer Dee <drummer.deja@gmail.com>
2022-09-03 23:48:34 -0400

    small changes to forgot controllers

--- routes/main.js
+++ routes/main.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth'); 
+const homeController = require('../controllers/home');
 const forgotController = require('../controllers/forgot');
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-router.get('/', homeController.getIndex)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
-router.get('/forgot-password', forgotController.getForgot)
-// app.post('/forgot-password',(req,res,next) => {
-
-// });
-
-// app.get('/reset-password',(req,res,next) => {
+router.get('/', homeController.getIndex);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.get('/logout', authController.logout);
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
+router.get('/forgot-password', forgotController.getForgot);
+// router.post('/forgot-password',(req,res,next) => {
 
 // });
+
+// router.get('/reset-password', resetController.getReset);
 
 // app.post('/reset-password', (req,res,next) => {
 
