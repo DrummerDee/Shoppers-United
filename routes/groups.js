e4f7566900e6dce969fc5f0cbf550f9b42e8b35d
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-09-06 00:13:47 +0300

    admin can delete user form group by userId, then automatically delete all that user todos in that group

--- routes/groups.js
+++ routes/groups.js
@@ -33,15 +33,30 @@ router.post("/createGroup", groupsController.createGroup);
 // router.get('/', ensureAuth, todosController.getTodos)
 router.get("/:groupId", ensureAuth, todosController.getGroupTodos);
 router.post("/createTodo/:groupId", todosController.createTodo);
+
 // show add user form
 router.get(
   "/addUser/:groupId",
   ensureAuth,
   groupsController.showAddUserTogroup
 );
+// add user to a specific group
+
+// show delete user form
+router.get(
+  "/deleteUser/:groupId",
+  ensureAuth,
+  groupsController.showDeleteUserFromGroup
+);
 
 // add user to a specific group
 router.post("/addUserToGroup/:groupId", groupsController.addUserToGroup);
+
+// delete user from a specific group
+router.post(
+  "/deleteUserFromGroup/:groupId",
+  groupsController.deleteUserFromGroup
+);
 
 router.put("/markComplete", todosController.markComplete);
 
