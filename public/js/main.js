3adefed2a1e920585f8da5d19b9892ed27766320
Drummer Dee <drummer.deja@gmail.com>
2022-09-05 15:43:27 -0400

    fixed conflicts

--- public/js/main.js
+++ public/js/main.js
@@ -147,5 +147,13 @@ async function markUnshared() {
 }
 
 
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye / eye slash icon
+  this.classList.toggle('fa-eye');
+});
+
 
 
