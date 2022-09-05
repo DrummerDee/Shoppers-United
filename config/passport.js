67e789a55bcae73a7c8c926d2ae60c8cfa2e9281
calimendoza <mbaivictor1@gmail.com>
2022-09-05 00:12:08 -0700

    user can signup with username or email

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,41 @@
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+  passport.use(new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+    var criteria = (username.indexOf('@') === -1) ? {username: username} : {email: username};
+    User.findOne({ username: username.toLowerCase() }, (err, user) => {
       if (err) { return done(err) }
-      if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` })
-      }
+      if(!user) {return done(null, false, {message: 'Incorrect username.'}); }
+      
       if (!user.password) {
         return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
       }
-      user.comparePassword(password, (err, isMatch) => {
+      
+      bcrypt.comparePassword(password, (err, isMatch) => {
         if (err) { return done(err) }
         if (isMatch) {
           return done(null, user)
         }
-        return done(null, false, { msg: 'Invalid email or password.' })
+        return done(null, false, { msg:'Invalid username or password.' })
       })
     })
   }))
-  
 
-  passport.serializeUser((user, done) => {
+
+  
+      
+     
+  
+  
+passport.serializeUser((user, done) => {
     done(null, user.id)
   })
 
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user))
-  })
+  });
+    
 }
--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
+const bcrypt = require('bcrypt')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -18,6 +20,8 @@ require('./config/passport')(passport)
 connectDB()
 
 app.set('view engine', 'ejs')
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -38,6 +42,43 @@ app.use(passport.session())
 
 app.use(flash())
   
+
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next()
+  res.redirect('/login')
+}
+
+function isLoggedOut(req, res, next){
+  if(!req.isAuthenticated()) return next()
+  res.redirect('/logout')
+}
+
+app.get('/', isLoggedIn, (req, res) => {
+  res.render("index", {title: "Home"});
+})
+
+app.get('/login', isLoggedOut, (req, res) => {
+
+  const response = {
+      title: "login",
+      error: req.query.error
+  }
+  res.render('login', response)
+} );
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login?error=true'
+}));
+
+app.get('logout', function (req, res) {
+  req.logout();
+  res.redirect('/')
+});
+
+
+
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
  
