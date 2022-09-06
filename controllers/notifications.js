42e80f84ab6963c76916876094b954bf8d698718
Jamie Cha <chaeunhee98@gmail.com>
2022-09-05 19:21:47 -0500

    adding basic features to app settings

---
+++ controllers/notifications.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+const notifications = require('../models/Notifications');
+const userProfile = require('../models/User')
+
+
+
+
+
+
+//My notes:  The controller responds to the user input and performs interactions on the data model objects.
